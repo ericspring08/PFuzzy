bfd8f01ad3bd64482cf607132bd79f8ae120acf6
Eric Zhang <ericspring08@gmail.com>
2022-06-01 13:58:35 -0500

    updated code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ wikipedia_fuzzer = Fuzzer()
 wikipedia_fuzzer.fuzz("wikipedia.page", ["String"], 10)
 wikipedia_fuzzer.fuzz("wikipedia.summary", ["String"], 10)
 wikipedia_fuzzer.fuzz("wikipedia.summary", ["String", "Integer"], 10)
-wikipedia_fuzzer.print_crash_logs()
 
 art_fuzzer = Fuzzer()
 art_fuzzer.fuzz("tprint", ["String"], 100)
